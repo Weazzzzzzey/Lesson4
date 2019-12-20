98ea3a54bf0f703e1d6444ad4d24cc478bd9b246
Weazzzzzzey <heronightmares@gmail.com>
2019-12-20 19:01:29 +0200

    Prideta pirma programa

--- SavarankiskiDarbai/SavarankiskiDarbai/Program.cs
+++ SavarankiskiDarbai/SavarankiskiDarbai/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace SavarankiskiDarbai
 {
+    public class NamuDarbai
+    {
+        public void TipoF()
+        {
+
+            int breikas = Convert.ToInt32(Console.ReadLine());
+            int ats = 0;
+            if (breikas > 0)
+            {
+
+                for (int i = 0; i <= breikas; i++)
+                {
+                    ats = ats + i;
+                }
+            }
+            else
+            {
+                for (int i = 0; i >= breikas; i--)
+                {
+                    ats = ats + i;
+                }
+            }
+            //Console.WriteLine(breikas);
+            Console.WriteLine(ats);
+            Console.ReadKey();
+        }
+        
+
+    }
+
+    
     class Program
     {
         static void Main(string[] args)
         {
+            NamuDarbai D1 = new NamuDarbai();
+            D1.TipoF();
         }
     }
 }
