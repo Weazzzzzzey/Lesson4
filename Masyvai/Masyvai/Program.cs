9d2c84fa9fe421827f2e65fdf795c9a3df1ecb70
Weazzzzzzey <heronightmares@gmail.com>
2019-12-20 20:21:42 +0200

    Prideti du forai ir masyvas

---
+++ Masyvai/Masyvai/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masyvai
+{
+    public class Masyvai
+    {
+        public void MasyvasF()
+        {
+
+            string[] SavaitesD =
+            {
+                "Pirmadienis",
+                "Antradienis",
+                "Treciadienis",
+                "Ketvirtadienis",
+                "Penktadienis",
+                "Sestadienis",
+                "Sekmadienis"
+            };
+
+            SavaitesD[2] = "TRECIADIENIS";
+            
+            foreach(string diena in SavaitesD)
+            {
+                Console.WriteLine(diena);
+            }
+
+            for (int i = 0; i < SavaitesD.Length;i++)
+            {
+                Console.WriteLine(SavaitesD[i]);
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Masyvai M = new Masyvai();
+            M.MasyvasF();
+        }
+    }
+}
