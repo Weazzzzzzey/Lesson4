f271071c6b3944abd9a934141a7312bba896da9d
Weazzzzzzey <heronightmares@gmail.com>
2019-12-22 14:13:59 +0200

    Pridetas 3 darbas su zodziu apvertimu

---
+++ Masyvo apvertimas/Masyvo apvertimas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasyvoApvertimas
+{ 
+    public class ZodziaiSakinyje 
+    {
+        
+        public void Apvertimas()
+        {
+            string Sakinys = "Hello world my name is Central park";
+            string[] MasZ = Sakinys.Split(' ');
+            
+            foreach(var a in MasZ.Reverse())
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZodziaiSakinyje M = new ZodziaiSakinyje();
+            M.Apvertimas();
+        }
+    }
+}
