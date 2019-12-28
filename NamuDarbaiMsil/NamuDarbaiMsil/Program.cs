7574f8d1e72f7468d50cc92e9d2a3a9b7c6d78dd
Weazzzzzzey <heronightmares@gmail.com>
2019-12-28 21:50:43 +0200

    Uzbaigtas appsas su Futbolu

--- NamuDarbaiMsil/NamuDarbaiMsil/Program.cs
+++ NamuDarbaiMsil/NamuDarbaiMsil/Program.cs
@@ -266,15 +266,55 @@ namespace NamuDarbaiMsil
 
         public void Futbolas()
         {
-            string results = " Manchester United 1 Chelsea 0, Arsenal 1 Manchester United 1, Manchester United 3 Fulham 1, Liverpool 2 Manchester United 1, Swansea 2 Manchester United 4";
+            string results = "Manchester United 1 Chelsea 0,Arsenal 1 Manchester United 1,Manchester United 3 Fulham 1,Liverpool 2 Manchester United 1,Swansea 2 Manchester United 4";
             string[] Games = results.Split(',');
-            
-            foreach (var item in Games)
-            {
-                Console.WriteLine(item);
-            }
+            string[] Itemai = new string[5];
+            int LaimetiM = 0;
+            int Lost = 0;
+            int Goal = 0;
+            int GoalIn = 0;
 
+            for (int i = 0; i < Games.Length; i++)
+            {
+                Itemai = Games[i].Split(' ');
+                
+                    if (Itemai[0].Contains("Manchester") && Itemai[1].Contains("United"))
+                    {
+                        if (Convert.ToInt32(Itemai[2]) > Convert.ToInt32(Itemai[4]))
+                        {
+                            LaimetiM++;
+                        }
+
+                        if (Convert.ToInt32(Itemai[2]) < Convert.ToInt32(Itemai[4]))
+                        {
+                            Lost++;
+                        }
+
+                        Goal = Goal + Convert.ToInt32(Itemai[2]);
+                        GoalIn = GoalIn + Convert.ToInt32(Itemai[4]);
+                    }
+                    else
+                    {
+                        if (Convert.ToInt32(Itemai[1]) < Convert.ToInt32(Itemai[4]))
+                        {
+                            LaimetiM++;
+                        }
+
+                        if (Convert.ToInt32(Itemai[1]) > Convert.ToInt32(Itemai[4]))
+                        {
+                            Lost++;
+                        }
+
+                        Goal = Goal + Convert.ToInt32(Itemai[4]);
+                        GoalIn = GoalIn + Convert.ToInt32(Itemai[1]);
+                    } 
+                    
+                
+            }
             Console.WriteLine("Games: {0}", Games.Length);
+            Console.WriteLine("Games Won: {0}", LaimetiM);
+            Console.WriteLine("Games Lost: {0}", Lost);
+            Console.WriteLine("Goals: {0}:{1}", Goal,GoalIn);
         }
     }
 
