f13ba4614cdd1e9e91b11742be204450eceb2fef
Weazzzzzzey <heronightmares@gmail.com>
2020-01-02 14:47:01 +0200

    Testo testavimo programa ir pats testas pridedami

---
+++ Bandymams/Bandymams/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bandymams
+{
+    class Bandymai
+    {
+        public void Testavimas()
+        {
+            bool hell = default;
+            byte a = 11, b = 22, c;
+            c = (byte)(a + b);
+            Console.WriteLine("Ats {0}",c);
+            Console.WriteLine(hell);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bandymai b = new Bandymai();
+            b.Testavimas();
+            Console.ReadKey();
+
+        }
+    }
+}
